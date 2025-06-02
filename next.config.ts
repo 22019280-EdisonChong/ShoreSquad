450596e11c15ef2d867f5cc0327becb5fc1b205c
22019280-EdisonChong <22019280@myrp.edu.sg>
2025-06-02 18:09:41 +0800

    Configure for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,16 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  reactStrictMode: true,
+  swcMinify: true,
   output: 'export',
+  basePath: isProd ? '/ShoreSquad' : '',
   images: {
-    unoptimized: true
-  },
-  basePath: process.env.NODE_ENV === 'production' ? '/c240-shoresquad' : ''
+    unoptimized: true,
+  }
 };
 
 export default nextConfig;
